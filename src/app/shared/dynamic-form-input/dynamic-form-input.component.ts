18df099de2c6df3431b0811b6c7868b6d862c651
beniniflavio <110288712+beniniflavio@users.noreply.github.com>
2023-05-06 07:16:59 -0300

    Fechamento balcao
    Fechamento balcão para itens básicos

--- src/app/shared/dynamic-form-input/dynamic-form-input.component.ts
+++ src/app/shared/dynamic-form-input/dynamic-form-input.component.ts
@@ -2,6 +2,7 @@ import { Component,  Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormField } from 'src/app/model/FormField';
 
+
 @Component({
   selector: 'app-dynamic-form-input',
   templateUrl: './dynamic-form-input.component.html',
@@ -13,4 +14,12 @@ export class DynamicFormInputComponent {
   @Input() form!: FormGroup;
 
   get isValid() { return this.form.controls[this.input.key].valid; }
+
+  getChange(event:any) {
+    var price = parseFloat(document.getElementById(event.target.id+"price")!.innerText) ;
+
+    document.getElementById(event.target.id+"amount")!.innerText = event.target.value + ' * ';
+    document.getElementById(event.target.id+"subtotal")!.innerText =(event.target.value*price).toFixed(2) ;
+
+  }
 }
