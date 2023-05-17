7cdeb7ae2afd2c8756434997900b4c4abfd6c44e
beniniflavio <110288712+beniniflavio@users.noreply.github.com>
2023-05-17 12:23:40 -0300

    Personalizados
    Finalizado venda de itens personalizado

--- src/app/consumo/personalizado/personalizado.service.ts
+++ src/app/consumo/personalizado/personalizado.service.ts
@@ -5,11 +5,22 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class PersonalizadoService {
-  private readonly APIMercadoria = 'http://localhost:8081/api/v1/Mercadoria/';
+  private readonly APIMercadoria = 'http://localhost:8081/api/v1/Consumo/Get/';
+  private readonly APIMercadoriaComponente = 'http://localhost:8081/api/v1/MercadoriaComponente/Get/';
+  private readonly APIMercadoriaAdicional = 'http://localhost:8081/api/v1/MercadoriaAdicional/Get/';
 
   constructor(private httpClient: HttpClient) {}
 
   getMercadoria(idhash: any) {
     return this.httpClient.get(this.APIMercadoria + idhash);
   }
+
+  getMercadoriaComponentes(idhash: any) {
+    return this.httpClient.get(this.APIMercadoriaComponente + idhash);
+  }
+
+  getMercadoriaAdicional(idhash: any) {
+    return this.httpClient.get(this.APIMercadoriaAdicional + idhash);
+  }
+
 }
